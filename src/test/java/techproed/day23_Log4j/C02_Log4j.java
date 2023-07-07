032a7c8b22ce3ea4d0cf3b54ffb3d063ba6d4738
Emre Yilmaz <emresamiylmz@gmail.com>
2023-07-07 23:03:31 +0300

    08.07.23

---
+++ src/test/java/techproed/day23_Log4j/C02_Log4j.java
@@ -0,0 +1,26 @@
+package techproed.day23_Log4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import techproed.utilities.TestBase;
+
+public class C02_Log4j extends TestBase {
+    @Test
+    public void test01() {
+        Logger logger = LogManager.getLogger(C02_Log4j.class);
+        //Techproeducation sayfasina gidelim
+        driver.get("https://www.techproeducation.com");
+        logger.info("Techproeducation sayfasina gidildi.");
+
+        driver.findElement(By.xpath("//i[@class='eicon-close']")).click();
+        logger.info("Sayfada cikan reklam kapatildi");
+
+        //Basligin Egitim icerdigini dogrulayalim
+        Assert.assertFalse(driver.getTitle().contains("Egitim"));
+        logger.error("Sayfa basligi Egitim yazisi icermiyor.");
+
+    }
+}
